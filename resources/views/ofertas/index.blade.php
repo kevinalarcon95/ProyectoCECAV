1fafec6ef2cc089daef037c71a014a52b6cc2a24
sergioorteera <sergioorteera@unicauca.edu.co>
2022-06-14 22:43:18 -0500

    Vista oferta e inscripciones

--- resources/views/ofertas/index.blade.php
+++ resources/views/ofertas/index.blade.php
@@ -1,53 +1,125 @@
-@extends('layouts.layout_home')
+@extends('layouts.template')
 
 
 @section('content')
 <!--Inicia sección Agro oferta-->
-<section name="oferta" id="oferta" class="container-fluid">
-    <div class="title text-center">
-        <strong>
-            <h1>Ofertas</h1>
-        </strong>
+<section name="oferta" id="oferta" class="container-fluid" style="display: fluid !important;">
+
+    <div class="row text-center mt-5 mb-3">
+        <h2 class="fw-bold">OFERTAS E INSCRIPCIONES</h2>
     </div>
-    <!--Barra de busqueda -->
+
+
     <div class="row">
+        <div class="col"></div>
         <div class="col">
-            <div class="container">
-                <div class="wrap">
-                    <div class="search">
-                        <input type="text" id="searchTerm" class="searchTerm" placeholder=" ¿Qué deseas buscar?">
-                        <button type="submit" id="buttonSearch" class="searchButton">
-                            <i class="fa fa-search"></i>
-                        </button>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Buscar" aria-describedby="button-addon2" style="background:#ECECEC;">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2" style="background:#04153B;">Buscar </button>
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+
+
+
+    <!--Barra de busqueda -->
+    <!--<div class="row">
+        <div class="col" id="search" style="position:absolute; top: 0;">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Buscar" aria-describedby="button-addon2" style="background:#ECECEC;">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2" style="background:#04153B;">Buscar </button>
+            </div>
+        </div>
+    </div>-->
+    <!--Fin Barra de busqueda -->
+
+
+    <!-- cards -->
+    <div class="row">
+
+        <div class="col-lg-4 col-sm-12 p-3">
+            <div class="card">
+                <img class="card-img-top" src={{ asset('img/medicinatradicionalchina_.jpg') }} alt="Card image cap">
+                <div class="card-body">
+
+                    <h5 class="card-text">Diplomado en Medicina
+                        tradicional china con énfasis en
+                        sus técnicas de acupuntura,
+                        masaje, moxibustión y Qi gong</h5>
+
+                    <p>Costo:</p>
+                    <p>Fecha de inicio:</p>
+                    <p>Fecha de finalizacion:</p>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <button type="button" class="btn btn-primary" style="background:#04153B; border:none;">Más detalles</button>
+                        </div>
+                        <div class="col-6 text-end">
+                            <button type="button" class="btn btn-primary" style="background:#004AAD; border:none;">Inscribirse</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!--Fin Barra de busqueda -->
 
-    <div class="row ml-5 mr-5 mb-4">
+        <div class="col-lg-4 col-sm-12 p-3">
+            <div class="card">
+                <img class="card-img-top" src={{ asset('img/medicinatradicionalchina_.jpg') }} alt="Card image cap">
+                <div class="card-body">
 
-    
-        <!-- inicio oferta -->
-        <div class="col-lg-4">
-            <div class="container" style="margin-top: 0px;">
-                <div class="content" style="padding-bottom: 12%;">
-                    <a href="#" target="_blank">  
-                    <div class="content-overlay">
+                    <h5 class="card-text">Diplomado en Medicina
+                        tradicional china con énfasis en
+                        sus técnicas de acupuntura,
+                        masaje, moxibustión y Qi gong</h5>
+
+                    <p>Costo:</p>
+                    <p>Fecha de inicio:</p>
+                    <p>Fecha de finalizacion:</p>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <button type="button" class="btn btn-primary" style="background:#04153B; border:none;">Más detalles</button>
+                        </div>
+                        <div class="col-6 text-end">
+                            <button type="button" class="btn btn-primary" style="background:#004AAD; border:none;">Inscribirse</button>
+                        </div>
                     </div>
-                    <img class="content-image" src="#">
-                    <div class="content-details fadeIn">
-                        <h3 class="content-title"></h3>
-                        <p class="content-description">Ver oferta</p>
-                    </div>
-                    </a>
                 </div>
             </div>
-        </div>        
-        <!-- fin oferta -->
-      
+        </div>
 
-    </div>    
+        <div class="col-lg-4 col-sm-12 p-3">
+            <div class="card">
+                <img class="card-img-top" src={{ asset('img/medicinatradicionalchina_.jpg') }} alt="Card image cap">
+                <div class="card-body">
+
+                    <h5 class="card-text">Diplomado en Medicina
+                        tradicional china con énfasis en
+                        sus técnicas de acupuntura,
+                        masaje, moxibustión y Qi gong</h5>
+
+                    <p>Costo:</p>
+                    <p>Fecha de inicio:</p>
+                    <p>Fecha de finalizacion:</p>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <button type="button" class="btn btn-primary" style="background:#04153B; border:none;">Más detalles</button>
+                        </div>
+                        <div class="col-6 text-end">
+                            <button type="button" class="btn btn-primary" style="background:#004AAD; border:none;">Inscribirse</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- fin cards -->
+
+    </div>
 </section>
 @endsection
